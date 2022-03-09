fd8628b790083fa9f66a753ee1029821bb110832
Eber <52986565+doneber@users.noreply.github.com>
2022-03-08 23:50:29 -0400

    Fix indentation in CardComponent

--- src/components/Card/CardComponent.tsx
+++ src/components/Card/CardComponent.tsx
@@ -3,13 +3,12 @@ import { Card } from 'react-bootstrap'
 
 const CardComponent = () => {
     return (
-        
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="http://www.fiumsa.edu.bo/academia1.jpg" />
-  <Card.Body>
-    <Card.Title>Grado</Card.Title>
-  </Card.Body>
-</Card>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="http://www.fiumsa.edu.bo/academia1.jpg" />
+            <Card.Body>
+                <Card.Title>Grado</Card.Title>
+            </Card.Body>
+        </Card>
     )
 }
 
